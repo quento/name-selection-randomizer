9b6110d7bd7d4edfe26216edf930a9a116b880f1
QuentO <email@quentino.org>
2018-05-06 12:30:16 -0500

    Added step-3 code and prev-step--3 code to stop sound

--- js/app.js
+++ js/app.js
@@ -32,8 +32,10 @@ $(document).ready(function () {
 
     // List is globally accessable
     var $lists_obj_array = [];
+    var canvas, ctx;
 
     $("#save-step-2").on('click', function() {
+        
         // TODO: Save list to JSON data
         var $list_num = $('#list-num').val();
         
@@ -64,9 +66,49 @@ $(document).ready(function () {
         //SaveData($lists_obj_array);
 
         // Populate randomizer form
-        //PopulateRandomizerFields($lists_obj_array);
+        PopulateRandomizerFields($lists_obj_array);
         
-        addTextToCanvas($lists_obj_array);       
+        //addTextToCanvas($lists_obj_array);       
+    });
+
+    $("#save-step-3").on('click', function(){
+        
+        // Run marquee to scroll list
+        var list1anim = setTimeout(function(){
+            scrollList("displayList1");
+        }, 500);
+        //scrollList("displayList2");
+
+        // Randomly select winner by picking random index value
+        var list_array = $lists_obj_array[0].list[0]
+        var winner_index = Math.floor(Math.random() * list_array.length);
+        var random_winner = list_array[winner_index];
+        
+        console.log($lists_obj_array[0].list[0]);
+        console.log("Random winnner " + random_winner);
+
+        // Pop selected from list
+        list_array.splice(winner_index, 1);
+        
+        console.log(list_array);
+        // After 6 seconds, Show winner, Use setInterval
+        setTimeout(function(){
+            $('#list1-winners').html(random_winner);
+        }, 6000);
+        
+        // Stop marquee
+
+        // Reload list minus winner
+              
+        
+    });
+    
+    $('#prev-step-3').on('click', function(){
+        
+        $("#play_spin_sound")[0].pause();
+        $("#play_spin_sound").currentTime = 0;
+
+        $('.marquee').marquee('pause');
     });
 });
 
@@ -85,56 +127,95 @@ function PopulateRandomizerFields(lists_obj_array) {
         $('#displayName' + listNumber).html(lists_obj_array[i].listname);
         //$('#displayList' + listNumber).html(lists_obj_array[i].list.join("\n"));
         // Get the list from select box in step 1.
-        $('#displayList' + listNumber).html($('#list' + listNumber).val().replace(/(?:\r\n|\r|\n)/g,"<br>"));
-        
+        $('#displayList' + listNumber).html($('#list' + listNumber).val().replace(/(?:\r\n|\r|\n)/g,"<br>"));        
     }
+    runRandomizer(lists_obj_array);
 }
 
-function addTextToCanvas(lists_obj_array){
-    //console.log("lists_obj_array => " + lists_obj_array);
-    
+function RunTextTopToBottom() {
+    step++;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.save();
+    ctx.translate(canvas.width / 2, step);
+    ctx.fillText("John Doe", 0, 0);
+    ctx.fillText("Peter Pan", 0, 30);
+    ctx.fillText("Jane Jax", 0, 60);
+    ctx.fillText("Paul Young", 0, 90);
+    ctx.fillText("Matt Old", 0, 120);
+    ctx.restore();
+    if (step == steps)
+        step = 0;
+    if (step < steps)
+        var t = setTimeout('RunTextTopToBottom()', 15);
+}
+
+function addTextToCanvas(lists_obj_array){    
+    var item_num;
+
     for(var j = 0; j <= lists_obj_array.length-1; j++){     
         var line_spacing = 35; 
-
-        //console.log(j + " - " + lists_obj_array[j].list[0]);
-        var item_num = j + 1;
+        // change number from zero based to work with content ids.
+        item_num = j + 1;
         // Create the canvas object
-        var canvas = document.getElementById("displayList" + item_num);
+        canvas = document.getElementById("displayList" + item_num);
         //console.log(canvas);
-        var ctx = canvas.getContext("2d");
+        ctx = canvas.getContext("2d");
         
         // Display the list name
         $('#displayName' + item_num).html(lists_obj_array[j].listname);
 
         // Basic text settings
-        ctx.textBasline = "top";
-        ctx.textAlign = "left";
-        ctx.font = '34px "Segoe UI"';
-        ctx.fillStyle = "rgba(0, 0, 0, 0.8)";
+        //ctx.textBasline = "top";
+        
+        //ctx.font = '34px "Segoe UI"';
+        //ctx.fillStyle = "rgba(0, 0, 0, 0.8)";
+        ctx.fillStyle = "blue";
+        ctx.font = "20pt Verdana";
+        //ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        step = 0;
+        steps = canvas.height + 10;
         
         for(var i = 0; i <= lists_obj_array[j].list[0].length-1; i++){            
             ctx.fillText(lists_obj_array[j].list[0][i], 0, line_spacing);
             line_spacing += 35;
         }
+        //RunTextTopToBottom(); 
+               
     }      
 }
+
+
+
 function runRandomizer(lists_obj_array){
    // Make canvas scroll, fast then slow
 
-   // Select item from array
+   // Randomly Select item from array
+
+   // Pop selected item from array
+
+   // Update the canvas list displayed
 }
 
-function scrollList(elem, speed) {
-    $(elem).animate(
-        {
-            marginTop: "300px"
-        },
-        {
-        duration: speed,
-        complete: function() {
-            $(elem).css("marginTop", "-450px");
-            scrollList(elem, speed);
-        }
+function scrollList(element_name) {
+    // Speed up marquee
+    $('#' + element_name)
+    .bind('beforeStarting', function(){
+        // play spinning wheel audio
+        $("#play_spin_sound")[0].play();
+    })
+    .bind('finished', function(){
+        // play winner found audio
+        $("#play_winner_sound")[0].play();
+    })
+    .marquee({ 
+        duration: 500,
+        speed:850,
+        //gap: 50,
+        delayBeforeStart: 0,        
+        direction: 'down',        
+        duplicated: false,
+        //startVisible: true,                
     });
 }
 
