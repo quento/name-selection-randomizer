cb9f9c75b17ace3a5f8b7893da6b2ee4761012e4
QuentO <email@quentino.org>
2018-05-07 00:09:08 -0500

    cleaned up each click event step of the randomizer app. Removed additional commented out code.

--- js/app.js
+++ js/app.js
@@ -4,14 +4,13 @@ $(document).ready(function () {
     //Initialize tooltips
     $('.nav-tabs > li a[title]').tooltip();
     
-    //Wizard
+    /************************** Wizard Behavior  **************************/
     $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
         var $target = $(e.target);    
         if ($target.parent().hasClass('disabled')) {
             return false;
         }
     });
-
 
     $(".next-step").click(function (e) {
         var $active = $('.wizard .nav-tabs li.active');
@@ -23,17 +22,13 @@ $(document).ready(function () {
         var $active = $('.wizard .nav-tabs li.active');
         prevTab($active);
     });
+    
+    
+    /***********************************************************************/
+    /************************** Name Randomizer  ***************************/
+    /***********************************************************************/
 
-    // Determine # of list boxes to show based on selection.
-    $("#save-step-1").on('click', function(){
-        var $list_num = $('#list-num').val();
-        hideLists($list_num);
-        if ($list_num > 1)        
-            showList($list_num);
-    });
-
-    /************************** Name Randomizer  **************************/
-    // List is globally accessable
+    // Container to hold list objects
     var $lists_obj_array = [];
     // Individual lists used per session
     var $list1 = [],
@@ -41,87 +36,82 @@ $(document).ready(function () {
         $list3 = [], 
         $list4 = [], 
         $list5 = [], 
-        $list6 = [];
-    var canvas, ctx;
-    
-    
+        $list6 = []; 
 
+    /************* Step 1 - Select Number of Lists ****************************/    
+    $("#save-step-1").on('click', function(){
+        var $list_num = $('#list-num').val();
+        hideLists($list_num);
+        if ($list_num > 1)        
+            showList($list_num);
+    });
+
+    /************* Step 2 - Add Lists ****************************/
     $("#save-step-2").on('click', function() {
         
-        // TODO: Save list to JSON data
+        // Save Step 1 Number of lists
         var $list_num = $('#list-num').val();
         
-        // console.log("Array Length before " + $lists_obj_array.length);
-        // Use list length as array length
-        // Save list and title as single item in multidimensional array.
-        for(var i = 0; i <= $list_num-1; i++) {
-            var num = i + 1;
-            $lists_obj_array[i] = {
-                "number": num,
-                "listname": $('#listName' + num).val(),
-                "list": [ 
-                    CreateListArray($('#list' + num).val())//.split("\n")
-                ],
-            };     
-            
-        }
-
-        // TROUBLESHOOTING - Look at the array
-        // console.log("Num items " + $list_num);
-        // console.log("Array Length after " + $lists_obj_array.length);
-        // console.log($lists_obj_array);
-        // $.each($lists_obj_array,function(index, value){
-        //     console.log(index + " - " + value);
-        // });
-
-        // Save to file
-        //SaveData($lists_obj_array);
+        // Save number, list and title as an object
+        $lists_obj_array = createListObjectsArray($list_num);
 
         // Populate randomizer form
         PopulateRandomizerFields($lists_obj_array);
-        
-        //addTextToCanvas($lists_obj_array);       
+          
     });
 
+    /************* Step 3 - Run Randomizer ***********************/
     $("#save-step-3").on('click', function(){
-        
+        var startScroll;
+        var current_list = [];
+        var winner_random_index = 0;
+        var random_winner,
+            unique_list,
+            winner_timeout_array = [],
+            current_num = 0,
+            showWin = [];
+            
         // Run marquee to scroll list
-        var startScroll = setTimeout(function(){ 
-                scrollList("displayList1"); 
+        startScroll = setTimeout(function(){ 
+                // Start scroll for all available items
+                for(var i = 1; i <= $lists_obj_array.length; i++) {
+                    scrollList("displayList" + i); 
+                }
             }, 
             500);
         
-        
-        // play sound
         $("#play_spin_sound")[0].play();
-        //scrollList("displayList2");
-
-        // Randomly select winner by picking random index value
-        $list1 = $lists_obj_array[0].list[0]
-        var winner_index = Math.floor(Math.random() * $list1.length);
-        var random_winner = $list1[winner_index];
-        
-        console.log($lists_obj_array[0].list[0]);
-        console.log("Random winnner " + random_winner);
-
-        // Pop selected from list
-        $list1.splice(winner_index, 1);
-        
-        console.log("Winner removed from list" + $list1);
-        // After 6 seconds, Show winner, Use setInterval
-        setTimeout(function(){
-            // Stop marquee
-            $("#step3 [id^='displayList'").marquee('pause');
+    
+        for(var i = 1; i <= $lists_obj_array.length; i++) {            
+            current_list = $lists_obj_array[i-1].list[0];
+            current_num = $lists_obj_array[i-1].number;
+            winner_random_index = Math.floor(Math.random() * current_list.length);
+            random_winner = current_list[winner_random_index];
             
-            $('#list1-winners').append(random_winner + "<br>");
-            // Update list with items minus winner
-            //$('#displayList1').html($list1.toString().replace("/(?:\,|\r|\n)/g","<br>"));  
-        },6500);
+            // Remove winner from the list
+            current_list.splice(winner_random_index, 1);
+            
+            winner_timeout_array[i] = setTimeout(function(){
+                    console.log("setTimeout occurring => " + i);
+                    // Stop all marquee
+                    $("#step3 [id^='displayList'").marquee('pause');
+                    
+                    //Play winner sound
+                    $("#play_winner_sound")[0].play();
         
-        
+                    // Display winner                     
+                    showWin[i] = showWinner("#displayList" + current_num, "<h3>" + random_winner + "</h3>");
+                    
+                    $('#list' + current_num + '-winners').append(random_winner + "<br>");
 
-        // Reload list minus winner
-              
+                },6500);  
+                
+                
+            // TROUBLESHOOTING - Test loop values
+            console.log(i + " Random winnner " + random_winner);
+            console.log("Winner removed from list" + current_list);            
+        }
+        //console.log(winner_timeout_array);
         
     });
     
@@ -137,52 +127,75 @@ $(document).ready(function () {
 
 /******************** Functions *************************/
 
+function createListObjectsArray($list_num){
+    var _list_obj_array = [],
+        num;
+
+    for(var i = 0; i <= $list_num-1; i++) {
+        num = i + 1;
+        
+        _list_obj_array[i] = {
+            "number": num,
+            "listname": $('#listName' + num).val(),
+            "list": [ 
+                CreateListArray($('#list' + num).val())//.split("\n")
+            ],
+        };     
+        
+    }
+    return _list_obj_array;
+}
+
 function PopulateRandomizerFields(lists_obj_array) {
-    console.log("Data to pop " + lists_obj_array);
+    console.log("Data to pop: ");
+    console.log(lists_obj_array);
 
     for(var i = 0; i <= lists_obj_array.length-1; i++) {
         var listNumber = lists_obj_array[i].number;
-        console.log("number = " + lists_obj_array[i].number);
-        console.log("listname = " + lists_obj_array[i].listname);
-        console.log("list = " + lists_obj_array[i].list.join("~"));
+        //console.log("number = " + lists_obj_array[i].number);
+        //console.log("listname = " + lists_obj_array[i].listname);
+        //console.log("list = " + lists_obj_array[i].list);
         // populate list views
         $('#displayName' + listNumber).html(lists_obj_array[i].listname);
         //$('#displayList' + listNumber).html(lists_obj_array[i].list.join("\n"));
         // Get the list from select box in step 1.
         $('#displayList' + listNumber).html($('#list' + listNumber).val().replace(/(?:\r\n|\r|\n)/g,"<br>"));        
     }
-    runRandomizer(lists_obj_array);
+    
 }
 
-function runRandomizer(lists_obj_array){
-   // Make canvas scroll, fast then slow
+function showWinner(containerID, msg_text){
+    //console.log("Container ID " + containerID);
+    $('<div class="winner">' + msg_text + '</div>').css({
+        position: "absolute",
+        width: "100%",
+        height: "100%",
+        top: 0,
+        left: 0,
+        background: "yellow",
+        padding: "35px 0",
+    }).appendTo($(containerID).css("position", "relative"));
+    
+    // Remove winner display after 3 seconds
+    var hide_winner = setTimeout(function(){
+        $(containerID + " .winner").slideUp();   
+        // Show winner box
+        $(containerID).siblings(".winner-container").slideDown();     
+    }, 3000);
 
-   // Randomly Select item from array
-
-   // Pop selected item from array
-
-   // Update the canvas list displayed
 }
 
 function scrollList(element_name) {
     // Speed up marquee
-    $('#' + element_name)
-    // .bind('beforeStarting', function(){
-    //     // play spinning wheel audio
-    //     $("#play_spin_sound")[0].play();
-    // })
-    // .bind('finished', function(){
-    //     // play winner found audio
-    //     $("#play_winner_sound")[0].play();
-    // })
+    $('#' + element_name)    
     .marquee({ 
-        duration: 500,
-        speed:850,
-        //gap: 50,
+        //duration: 3500,
+        speed:650,
+        //gap: 0,
         delayBeforeStart: 0,        
-        direction: 'down',        
-        duplicated: false,
-        //startVisible: true,                
+        direction: 'up',        
+        duplicated: true,
+        startVisible: true,                
     });
 }
 
